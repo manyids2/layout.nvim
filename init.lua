0895c624c3002ad19a7e226c3ccb511db05953b4
manyids2 <arun.mukundan@gmail.com>
2023-05-24 14:31:29 +0200

    Read config

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ require("layout").setup()
 
 local a = vim.api
 local app_open = a.nvim_create_augroup("app_open", { clear = true })
-a.nvim_create_autocmd({ "BufEnter" }, {
+a.nvim_create_autocmd({ "BufRead" }, {
   group = app_open,
   callback = function()
     require("layout").setup()
--- lua/layout/init.lua
+++ lua/layout/init.lua
@@ -4,9 +4,11 @@ local io = require("layout.io")
 local app = {}
 
 app.state = {
+	config = {},
 	open = {},
 	wins = {},
 	bufs = {},
+	active = "",
 }
 
 app.dashboard = [[
@@ -15,16 +17,47 @@ app.dashboard = [[
 
 ]]
 
+function app.load_config(self)
+	-- load config file using XDG_CONFIG_HOME
+	local output = a.nvim_exec2("!echo $XDG_CONFIG_HOME", { output = true }).output
+	self.xdg_dir = vim.split(output, "\n", {})[3]
+	self.src_dir = self.xdg_dir .. "/nvim-apps/layout.nvim"
+	local pathlist = vim.fs.find("config.yaml", { upward = false, path = self.src_dir })
+	if vim.tbl_count(pathlist) > 0 then
+		self.cfg_file = pathlist[1]
+
+		-- create a buffer for holding the config
+		vim.cmd("e " .. self.cfg_file)
+		local buf = a.nvim_get_current_buf()
+		local win = a.nvim_get_current_win()
+		a.nvim_buf_set_option(buf, "buflisted", false)
+
+		local s = self.state
+		s.bufs.config = buf
+		s.wins.config = win
+		table.insert(s.open, "config")
+
+		-- parse the config to tree
+		s.config = {}
+	else
+		return
+	end
+end
+
 function app.mount(self)
 	local s = self.state
 
 	-- Unlist empty buffer
 	local buf = a.nvim_get_current_buf()
+	local win = a.nvim_get_current_win()
+	s.wins.dashboard = win
+	s.bufs.dashboard = buf
 	a.nvim_buf_set_option(buf, "buflisted", false)
 
 	-- Display dashboard
+	s.active = "dashboard"
+	s.open = { "dashboard" }
 	io.set_text(buf, self.dashboard)
-	s.bufs.config = buf
 
 	-- Destroy on quit
 	vim.keymap.set("n", "q", function()
@@ -44,12 +77,19 @@ function app.setup()
 	local filename = a.nvim_exec2("echo expand('%')", { output = true }).output
 	if filename == "" then
 		app:mount()
+		app:load_config()
+		P(app.cfg_file)
 		return
 	end
+
 	local s = app.state
 	table.insert(s.open, filename)
+	local win = a.nvim_get_current_win()
+	local buf = a.nvim_get_current_buf()
+	s.wins[filename] = win
+	s.bufs[filename] = buf
+	s.active = filename
 
-	P(app.state)
 	return app
 end
 
